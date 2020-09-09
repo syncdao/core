622f81814893d22d120776b2da71f22c3e42b097
Nour Haridy <nourharidy@yahoo.com>
2020-09-09 08:33:04 +0200

    add rkDai staking

--- contracts/rkDaiStaking.sol
+++ contracts/rkDaiStaking.sol
@@ -1,9 +1,175 @@
 pragma solidity ^0.5.16;
 
-import "./Staking.sol";
+/*
+MIT License
 
-contract RkDaiStaking is StakingRewards {
+Copyright (c) 2019 Synthetix
 
-    constructor(address syncAddress, address rkdaiAddress, uint startDate) StakingRewards(syncAddress, rkdaiAddress, 21 days, startDate) public {}
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+import "openzeppelin-solidity-2.3.0/contracts/math/Math.sol";
+import "openzeppelin-solidity-2.3.0/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity-2.3.0/contracts/token/ERC20/ERC20Detailed.sol";
+import "openzeppelin-solidity-2.3.0/contracts/token/ERC20/SafeERC20.sol";
+import "openzeppelin-solidity-2.3.0/contracts/utils/ReentrancyGuard.sol";
+
+contract StakingRewards is ReentrancyGuard {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    /* ========== STATE VARIABLES ========== */
+
+    IERC20 public rewardsToken;
+    address public stakingController;
+    uint256 public periodFinish = 0;
+    uint256 public rewardRate = 0;
+    uint256 public rewardsDuration;
+    uint256 public lastUpdateTime;
+    uint256 public rewardPerTokenStored;
+    uint public startDate;
+    bool public started = false;
+
+    mapping(address => uint256) public userRewardPerTokenPaid;
+    mapping(address => uint256) public rewards;
+
+    uint256 private _totalSupply;
+    mapping(address => uint256) private _balances;
+
+    /* ========== CONSTRUCTOR ========== */
+
+    constructor(
+        address _rewardsToken,
+        address _stakingController,
+        uint _rewardsDurationSeconds,
+        uint _startDate
+    ) public {
+        rewardsToken = IERC20(_rewardsToken);
+        stakingController = _stakingController;
+        rewardsDuration = _rewardsDurationSeconds;
+        startDate = _startDate;
+    }
+
+    /* ========== VIEWS ========== */
+
+    function totalSupply() external view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return _balances[account];
+    }
+
+    function lastTimeRewardApplicable() public view returns (uint256) {
+        return Math.min(block.timestamp, periodFinish);
+    }
+
+    function rewardPerToken() public view returns (uint256) {
+        if (_totalSupply == 0) {
+            return rewardPerTokenStored;
+        }
+        return
+            rewardPerTokenStored.add(
+                lastTimeRewardApplicable().sub(lastUpdateTime).mul(rewardRate).mul(1e18).div(_totalSupply)
+            );
+    }
+
+    function earned(address account) public view returns (uint256) {
+        return _balances[account].mul(rewardPerToken().sub(userRewardPerTokenPaid[account])).div(1e18).add(rewards[account]);
+    }
+
+    function getRewardForDuration() external view returns (uint256) {
+        return rewardRate.mul(rewardsDuration);
+    }
+
+    /* ========== MUTATIVE FUNCTIONS ========== */
+
+    function stake(address user, uint256 amount) external onlyController updateReward(user) {
+        require(amount > 0, "Cannot stake 0");
+        _totalSupply = _totalSupply.add(amount);
+        _balances[user] = _balances[user].add(amount);
+        emit Staked(user, amount);
+    }
+
+    function withdraw(address user, uint256 amount) public onlyController updateReward(user) {
+        require(amount > 0, "Cannot withdraw 0");
+        _totalSupply = _totalSupply.sub(amount);
+        _balances[user] = _balances[user].sub(amount);
+        emit Withdrawn(user, amount);
+        // We also send user his reward on each withdrawal
+        uint256 reward = rewards[user];
+        if (reward > 0) {
+            rewards[user] = 0;
+            rewardsToken.safeTransfer(user, reward);
+            emit RewardPaid(user, reward);
+        }
+    }
+
+    function getReward() public nonReentrant updateReward(msg.sender) {
+        uint256 reward = rewards[msg.sender];
+        if (reward > 0) {
+            rewards[msg.sender] = 0;
+            rewardsToken.safeTransfer(msg.sender, reward);
+            emit RewardPaid(msg.sender, reward);
+        }
+    }
+
+    function start() external updateReward(address(0)) {
+        require(block.timestamp >= startDate, "startDate has not yet been reached");
+        require(!started, "Distribution has already started");
+        uint reward = rewardsToken.balanceOf(address(this));
+        require(reward > 0, "No tokens were sent for rewards");
+        rewardRate = reward.div(rewardsDuration);
+
+        // Ensure the provided reward amount is not more than the balance in the contract.
+        // This keeps the reward rate in the right range, preventing overflows due to
+        // very high values of rewardRate in the earned and rewardsPerToken functions;
+        // Reward + leftover must be less than 2^256 / 10^18 to avoid overflow.
+        require(rewardRate <= reward.div(rewardsDuration), "Provided reward too high");
+
+        lastUpdateTime = block.timestamp;
+        started = true;
+        periodFinish = block.timestamp.add(rewardsDuration);
+        emit Started(reward);
+    }
+
+    /* ========== MODIFIERS ========== */
+
+    modifier updateReward(address account) {
+        rewardPerTokenStored = rewardPerToken();
+        lastUpdateTime = lastTimeRewardApplicable();
+        if (account != address(0)) {
+            rewards[account] = earned(account);
+            userRewardPerTokenPaid[account] = rewardPerTokenStored;
+        }
+        _;
+    }
+
+    modifier onlyController() {
+        require(msg.sender == stakingController, 'Only callable by controller');
+        _;
+    }
+
+    /* ========== EVENTS ========== */
+
+    event Started(uint256 reward);
+    event Staked(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
+    event RewardPaid(address indexed user, uint256 reward);
 }
